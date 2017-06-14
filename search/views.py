8bbbaac3d3e2fc1ae35b34d4c83d9160d701813c
MicrobesNG <microbesng@bio1187.bham.ac.uk>
2017-06-14 14:18:03 +0100

    search views and urls

--- search/views.py
+++ search/views.py
@@ -4,3 +4,19 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def search(request):
+
+    return render(request, "search/search.html", {})
+
+
+def results(request):
+
+
+    return render(request, "search/results.html", {})
+
+
+def details(request, strain_id):
+
+    return render(request, "search/details.html", {})
