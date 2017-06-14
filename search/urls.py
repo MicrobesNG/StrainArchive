5b718db06479f1bace8b3c9eae701f8d94a08cb9
MicrobesNG <microbesng@bio1187.bham.ac.uk>
2017-06-14 14:59:45 +0100

    allow query to be passed in results url

--- search/urls.py
+++ search/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^search/', views.search, name = 'search'),
     url(r'^post/(?P<strain_pk>\d+)/$', views.details, name = 'details'),
-    url(r'^results/', views.results, name = 'results')
+    url(r'^results/$', views.results, name = 'results')
 ]
