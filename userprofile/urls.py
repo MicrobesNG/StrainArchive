ae5cee0f71a7d961bc7be0fca7f83a97357683f5
MicrobesNG <microbesng@bio1187.bham.ac.uk>
2017-06-14 11:02:27 +0100

    login specific url added

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.login_page, name = "login"),
+    url(r'^login/', views.login_page, name = "login"),
     url(r'^dashboard/', views.user_dashboard, name = "dashboard"),
 ]
