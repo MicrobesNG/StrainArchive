5568d8a05cd1b4fe1fe8f5847e4daa1ee2237b5e
MicrobesNG <microbesng@bio1187.bham.ac.uk>
2017-06-14 15:04:53 +0100

    urls

--- StrainArchive/urls.py
+++ StrainArchive/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^management/', include('management.urls', namespace = "management")),
     url(r'^user/', include('userprofile.urls', namespace = "userprofile")),
+    url(r'^search/', include('search.urls', namespace = "search")),
     url(r'^', include('archive.urls', namespace = "archive"))
 ]
--- search/urls.py
+++ search/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^search/', views.search, name = 'search'),
+    url(r'^', views.search, name = 'search'),
     url(r'^post/(?P<strain_pk>\d+)/$', views.details, name = 'details'),
     url(r'^results/$', views.results, name = 'results')
 ]
