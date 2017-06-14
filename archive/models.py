84c757356ba0484ee11074d7ced9d1d1d2dbd248
MicrobesNG <microbesng@colmds-c02qr0br.home>
2017-06-14 16:46:37 +0100

    added taxonomical models to make classification/searching faster

--- archive/models.py
+++ archive/models.py
@@ -8,10 +8,36 @@ from datetime import datetime
 from userprofile.models import Organisation
 
 
+# taxonomical models
+class Family(models.Model):
+    name = models.CharField(max_length = 100)
+    
+    # other family data fields?
+
+class Genus(models.Model):
+    name = models.CharField(max_length = 100)
+    
+    # other genus data fields?
+
+class Species(models.Model):
+    name = models.CharField(max_length = 100)
+    
+    # other species data fields? 
+
 class Strain(models.Model):
 
+    STRAIN_TYPES = (
+        ("D", "Default"),
+        ("S", "SomeotherType")
+    )
+
     name = models.CharField(max_length = 100)
+    strain_type = models.CharField(max_length = 1, choices = STRAIN_TYPES, default = "D")
     date_added = models.DateTimeField(default = datetime.now)
     organisation = models.ForeignKey(Organisation, null = True)
     data_url = models.URLField(null = True)
     number_of_sales = models.IntegerField(default = 0)
+
+    family = models.ForeignKey(Family, null = True)
+    genus = models.ForeignKey(Genus, null = True)
+    species = models.ForeignKey(Species, null = True)
