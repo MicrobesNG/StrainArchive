518f9c81485eb069a3e45a87ac23c79fcee7e9b0
MicrobesNG <microbesng@bio1187.bham.ac.uk>
2017-06-20 11:41:45 +0100

    update basket

--- cart/static/js/basket.js
+++ cart/static/js/basket.js
@@ -12,18 +12,81 @@ function generateBasketCostText(amount) {
     return "£" + amount.toString();
 }
 
-function setBasketCostText() {
-    var amount = calculateBasketTotalCost();
+function setBasketCostText(amount) {
+    // var amount = calculateBasketTotalCost();
     var text = generateBasketCostText(amount.toFixed(2));
     $(BASKET_COST_TEXT_SELECTOR).html(text);
 }
 
 
+function updateRowInBasket(row, newAmount, newCost) {
+    
+    row.find(".itemAmountCell").html(newAmount);
+    row.find(".itemCostCell").html(newCost);
+
+}
+
+function generateBasketEntryRow(newName, newAmount, newCost) {
+
+    var row = '<tr class="itemRow">';
+    row += '<td class="removeItemCell"><button type="button" class="btn btn-xs btn-danger removeFromBasket">&times;</button></td>';
+    row += '<td class="itemNameCell">' + newName + '</td>';
+    row += '<td class="itemAmountCell">' + newAmount + '</td>';
+    row += '<td class="itemCostCell">' + newCost + '</td>';
+    row += '<tr>';
+
+    return row;
+}
+
+
+function updateBasket(data) {
+    
+    for (var i = 0; i < data["items"].length; i++) {
+        
+        var currentSessionStrain = data["items"][i];
+        
+        var located = false;
+        $(".itemRow").each(function() {
+
+            var basketName = $(this).find(".itemNameCell").html();
+            var basketAmount = $(this).find(".itemAmountCell").html();
+            var basketCost = $(this).find(".itemCostCell").html();
+
+            if (basketName == currentSessionStrain["name"]) {
+                located = true;
+
+                updateRowInBasket(
+                    $(this),
+                    currentSessionStrain["amount"],
+                    currentSessionStrain["cost"]
+                );
+
+                break;
+            }
+        });
+
+        if (!located) {
+
+            var html = generateBasketEntryRow(
+                currentSessionStrain["name"],
+                currentSessionStrain["amount"],
+                currentSessionStrain["cost"]
+            );
+
+        }
+
+    }
+
+    setBasketCostText(data["total_cost"]);
+
+}
+
+
 function addToBasket(strainPK) {
     $.ajax({
-        url: "someurl",
-        success: function() {
-            // somefunction
+        url: "/add_to_basket/" + strainPK,
+        success: function(data) {
+            updateBasket(data);
         }
     });
 }
--- cart/views.py
+++ cart/views.py
@@ -2,10 +2,42 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
-# Create your views here.
+from archive.models import Strain
+from . import utils
 
 
 def add_to_basket(request, strain_pk):
 
-    pass
+    try:
+
+        selected_strain = Strain.objects.get(pk = strain_pk)
+
+    except Strain.DoesNotExist:
+
+        pass
+
+    else:
+
+        utils.add_to_basket(request, selected_strain)
+
+    return utils.get_basket(request)
+
+
+
+
+
+def remove_from_basket(request, strain_pk):
+
+    try:
+
+        selected_strain = Strain.objects.get(pk = strain_pk)
+    
+    except Strain.DoesNotExist:
+
+        pass
+
+    else:
+
+        utils.remove_from_basket(request, selected_strain)
+
+    return utils.get_basket(request)
