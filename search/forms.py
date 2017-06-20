3adac582addeca6ed478ff1d4fbe34055897fd82
MicrobesNG <microbesng@bio1187.bham.ac.uk>
2017-06-20 09:06:13 +0100

    move basket functionality into cart app

--- search/forms.py
+++ search/forms.py
@@ -7,52 +7,6 @@ import urllib
 from archive.models import Strain, Family, Genus, Species
 
 
-class RemoveFromBasket(forms.Form):
-
-    strain_pk = forms.IntegerField(required = True)
-
-    def process(self, request):
-        
-        cleaned_strain_pk = self.cleaned_data["strain_pk"]
-
-        try:
-
-            strain = Strain.objects.get(pk = cleaned_strain_pk)
-
-        except Strain.DoesNotExist:
-
-            pass
-
-        else:
-
-            for strain in request.session["basket"]["items"]:
-
-                pass
-
-
-
-
-class AddToBasket(forms.Form):
-
-    strain_pk = forms.IntegerField(required = True)
-
-    def process(self, request):
-
-        cleaned_strain_pk = self.cleaned_data["strain_pk"]
-
-        try:
-
-            strain = Strain.objects.get(pk = cleaned_strain_pk)
-
-        except Strain.DoesNotExist:
-
-            pass
-
-        else:
-        
-            request.session["basket"]["items"].append({"name": strain.name, "amount": 3, "cost": 50})
-
-
 
 
 
