f9da61ec1ecebd6e50dad3fe0404e8e40bd02e3e
MicrobesNG <microbesng@COLMDS-C02QR0BR.local>
2017-06-21 16:09:33 +0100

    submit quote form

--- cart/forms.py
+++ cart/forms.py
@@ -1,7 +1,34 @@
 from django import forms
 from archive.models import Strain
-
+import json
 
 class QuoteForm(forms.Form):
 
-    pass
+        FUNDING_TYPES = (
+            
+        )
+
+        name = forms.CharField(max_length = 100, required = True)
+        email = forms.EmailField(required = True)
+        billing_address = forms.CharField(required = True, max_length = 200)
+        delivery_address = forms.CharField(required = True, max_length = 200)
+        funding_type = forms.CharField(required = True, max_length = 1)
+
+    
+    def process(self, request):
+
+        cleaned_name = self.cleaned_data["name"]
+        cleaned_email = self.cleaned_data["email"]
+        cleaned_billing_address = self.cleaned_data["billing_address"]
+        cleaned_delivery_address = self.cleaned_data["delivery_address"]
+        cleaned_funding_type = self.cleaned_data["funding_type"]
+
+        
+
+    
+    def process_errors(self, request):
+        
+        error_dict = json.loads(self.errors.as_json())
+        for key in error_dict:
+            for error in error_dict[key]:
+                messages.error(request, "Error: %s - %s" % (key, error["message"]))
