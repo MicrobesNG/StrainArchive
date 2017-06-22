8f5b8a0c41925b5b6987f0b6a588f817b51d5bc4
MicrobesNG <microbesng@COLMDS-C02QR0BR.local>
2017-06-22 08:41:17 +0100

    quoteform fields

--- cart/forms.py
+++ cart/forms.py
@@ -1,18 +1,24 @@
 from django import forms
+from django.conf import messages
 from archive.models import Strain
 import json
 
 class QuoteForm(forms.Form):
 
         FUNDING_TYPES = (
-            
+            ("NC", "Non-Commercial"),
+            ("B", "BBSRC"),
+            ("I", "Industry"),
+            ("UB", "Internal UoB")
         )
 
         name = forms.CharField(max_length = 100, required = True)
         email = forms.EmailField(required = True)
         billing_address = forms.CharField(required = True, max_length = 200)
         delivery_address = forms.CharField(required = True, max_length = 200)
-        funding_type = forms.CharField(required = True, max_length = 1)
+        funding_type = forms.CharField(required = True, max_length = 2)
+        bbsrc_code = forms.CharField(required = False, max_length = 10)
+        note = forms.CharField(required = False, max_length = 250)
 
     
     def process(self, request):
@@ -22,6 +28,8 @@ class QuoteForm(forms.Form):
         cleaned_billing_address = self.cleaned_data["billing_address"]
         cleaned_delivery_address = self.cleaned_data["delivery_address"]
         cleaned_funding_type = self.cleaned_data["funding_type"]
+
+
 
         
 
