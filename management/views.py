8ac1f05a424c7189adea9b9f8a5352b761cc52a0
MicrobesNG <microbesng@COLMDS-C02QR0BR.local>
2017-06-23 09:56:08 +0100

    work on loading details for quotes and orders

--- management/views.py
+++ management/views.py
@@ -8,6 +8,62 @@ from django.contrib.auth.models import User
 from cart.models import Quote, Order, ConfirmedBasket, Purchase
 
 
+def get_order_details(request, order_pk):
+
+    try:
+    
+        order = Order.objects.get(pk = order_pk)
+    
+    except Order.DoesNotExist:
+    
+        messages.error(request, "Details for quote could not be located")
+        order = None
+    
+    else:
+
+        order_data = {
+
+        }
+        
+
+    return HttpResponse(
+        json.dumps(order_data),
+        content_type = "application/json"
+    )
+
+def get_quote_details(request, quote_pk):
+
+    try:
+    
+        quote = Quote.objects.get(pk = quote_pk)
+    
+    except Quote.DoesNotExist:
+    
+        messages.error(request, "Details for quote could not be located")
+        quote = None
+    
+    else:
+
+        quote_data = {
+            "customer_name": quote.customer_name,
+            "customer_email": quote.customer_email,
+            "funding_Type": quote.funding_type,
+            "bbsrc_code": quote.bbsrc_code,
+            "billing_address": quote.billing_address,
+            "selivery_address": quote.delivery_address,
+            "customer_notes": quote.customer_note
+        }
+        
+
+    return HttpResponse(
+        json.dumps(quote_data),
+        content_type = "application/json"
+    )
+
+
+
+
+
 def management_dashboard(request):
 
     
