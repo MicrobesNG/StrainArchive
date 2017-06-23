50f8d05d7ff128a8c68377d4169ddeb7cb943c9e
MicrobesNG <microbesng@COLMDS-C02QR0BR.local>
2017-06-23 15:31:12 +0100

    comments

--- cart/models.py
+++ cart/models.py
@@ -10,6 +10,7 @@ import json
 # how many of which strain to be purchased
 class Purchase(models.Model):
 
+    # which strain, how many, and total cost for purchase
     strain = models.ForeignKey(Strain)
     quantity = models.IntegerField(default = 0)
     cost = models.FloatField(default = 0.0)
@@ -17,10 +18,11 @@ class Purchase(models.Model):
 # collection of purchases and user which requested them
 class ConfirmedBasket(models.Model):
 
-    
+    # purchases in order and summed cost for all of them
     purchases = models.ManyToManyField(Purchase)
     total_cost = models.FloatField(default = 0.0)
 
+    # put data for basket into dict
     def as_dict(self):
 
         output = {"purchases": [], "total_cost": self.total_cost}
@@ -37,6 +39,7 @@ class ConfirmedBasket(models.Model):
         
         return output
 
+    # serialize basket into json string
     def as_json_string(self):
 
         return json.dumps(self.as_dict())
@@ -45,6 +48,7 @@ class ConfirmedBasket(models.Model):
 # quote for cost of basket
 class Quote(models.Model):
 
+    # status of quote
     STATUS_CHOICES = (
         ("P", "Pending"),
         ("S", "Sent"),
@@ -53,6 +57,7 @@ class Quote(models.Model):
         ("C", "Cancelled")
     )
 
+    # available types of funding
     FUNDING_TYPES = (
         ("NC", "Non-Commercial"),
         ("B", "BBSRC"),
@@ -61,25 +66,29 @@ class Quote(models.Model):
         ("NS", "Not Set")
     )
     
-    status = models.CharField(
-        default = "P",
-        choices = STATUS_CHOICES,
-        max_length = 1
-    )
+    status = models.CharField(default = "P", choices = STATUS_CHOICES, max_length = 1)
+    funding_type = models.CharField(max_length = 2, choices = FUNDING_TYPES, default = "NS")
 
+    # bbsrc funding code (required if bbsrc funding put on quote)
+    bbsrc_code = models.CharField(max_length = 10, null = True)
+    
+    # customer details
+    # (note can be attached to quote by customer)
     customer_name = models.CharField(max_length = 100, null = True)
     customer_email = models.EmailField(null = True)
-    basket = models.OneToOneField(ConfirmedBasket, null = True)
-    funding_type = models.CharField(max_length = 2, choices = FUNDING_TYPES, default = "NS")
-    bbsrc_code = models.CharField(max_length = 10, null = True)
-
-    billing_address = models.TextField(default = "")
-    delivery_address = models.TextField(default = "")
     customer_note = models.TextField(default = "")
 
+    # basket for which quote relates to
+    basket = models.OneToOneField(ConfirmedBasket, null = True)
+    
+    # addresses
+    billing_address = models.TextField(default = "")
+    delivery_address = models.TextField(default = "")
+    
+    # date at which quote was created
     creation_date = models.DateTimeField(default = datetime.now)
 
-
+    # get display names for funding type and status
     def get_verbose_funding_type_name(self):
 
         return self.get_funding_type_display()
@@ -92,6 +101,7 @@ class Quote(models.Model):
 # represents dispatched product/order status
 class Order(models.Model):
 
+    # status of order
     STATUS_CHOICES = (
         ("P", "Processing"),
         ("D", "Dispatched"),
@@ -99,20 +109,21 @@ class Order(models.Model):
         ("V", "Void")
     )
 
-    quote = models.OneToOneField(Quote, null = True)
-    
     status = models.CharField(
         default = "A",
         choices = STATUS_CHOICES,
         max_length = 1
     )
 
+    # quote for which order is for
+    quote = models.OneToOneField(Quote, null = True)
 
+    # dates for each stage of order
     start_date = models.DateTimeField(default = datetime.now)
     post_date = models.DateTimeField(null = True)
     received_date = models.DateTimeField(null = True)
 
-
+    # get display name for order status
     def get_verbose_status_name(self):
 
         return self.get_status_display()
--- management/static/js/sales.js
+++ management/static/js/sales.js
@@ -1,44 +1,59 @@
+
+// populate quote details modal with data from backend
 function populateQuoteDetailsModal(data) {
 
+    // set customer details and funding type
     $("#quoteDetailsCustomerName").text(data["customer_name"]);
     $("#quoteDetailsCustomerEmail").text(data["customer_email"]);
     $("#quoteDetailsFundingType").text(data["funding_type"]);
+
+    // if bbsrc funding, display the bbsrc code
     if (data["bbsrc_code"] != "") {
         $("#quoteDetailsBBSRCCode").text("(" + data["bbsrc_code"] + ")");
     } else {
         $("#quoteDetailsBBSRCCode").text(data["bbsrc_code"]);
     }
+    
+    // display addresses
     $("#quoteDetailsBillingAddress").text(data["billing_address"]);
     $("#quoteDetailsDeliveryAddress").text(data["delivery_address"]);
+    
+    // display customer note if present
     $("#quoteDetailsCustomerNotes").text(data["customer_notes"]);
 
+    // iterate over purchases and add list element for each one
     for (var i = 0; i < data["basket"]["purchases"].length; i++) {
 
+        // get current purchase at index i
         var currentPurchase = data["basket"]["purchases"][i];
         
-        var row = "<li>";
-            row += currentPurchase["quantity"] + "x ";
-            row += currentPurchase["strain_name"] + " - ";
-            row += "£" + currentPurchase["cost"];
-            row += "</li>";
+        // construct html for element in list
+        var listElement = "<li>";
+            listElement += currentPurchase["quantity"] + "x ";
+            listElement += currentPurchase["strain_name"] + " - ";
+            listElement += "£" + currentPurchase["cost"];
+            listElement += "</li>";
 
-        $("#basketContentList").append(row);
+        // add new list element to list
+        $("#basketContentList").append(listElement);
 
     }
 
 }
 
-
+// send request for details of quote with pk = quotePK
 function getQuoteDetails(quotePK) {
     $.ajax({
         url: "/management/getQuoteDetails/" + quotePK,
         success: function(data) {
+            // populate and display quote details modal
             populateQuoteDetailsModal(data);
             $("#checkoutSummaryModal").modal("show");
         }
     });
 }
 
+// send request for details of order with pk = orderPK
 function getOrderDetails(orderPK) {
     $.ajax({
         url: "/management/getOrderDetails/" + orderPK,
@@ -48,12 +63,12 @@ function getOrderDetails(orderPK) {
     });
 }
 
-
+// when page is ready, execute this code
 $(document).ready(function() {
 
+    // get the details for quote on button click
     $(".viewQuote").click(function() {
-        getQuoteDetails($(this).parent().parent().attr("id"));
-        
+        getQuoteDetails($(this).parent().parent().attr("id"));    
     });
 
 });
