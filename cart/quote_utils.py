a0a9bcc61fd8b9b2049b1ab8575e383a6ed176c4
MicrobesNG <microbesng@COLMDS-C02QR0BR.local>
2017-06-28 12:41:45 +0100

    work on auto pdf generation

--- cart/quote_utils.py
+++ cart/quote_utils.py
@@ -7,30 +7,30 @@ from reportlab.pdfgen import canvas
 from xhtml2pdf import pisa
 import cStringIO as StringIO
 
-
+# uses django template to create a pdf file for quote
 def generate_quote_pdf(quote):
 
-    
+    # put quote into context dictionary for template
     context_dictionary = {
         "quote": quote
     }
 
+    # get the template
     template = get_template("pdfs/quote.html")
-    context = Context(context_dictionary)
+    # generate the html
     html = template.render(context_dictionary)
 
-    result = StringIO.StringIO()
-
-    pdf = pisa.pisaDocument(StringIO.StringIO(html.encode("ISO-8859-1")), result)
-
+    # open new file for writing pdf to
     new_file = open("quotes/test.pdf", "w")
     
+    # generate pdf and write to new file
     pisaStatus = pisa.CreatePDF(
         html.encode('utf-8'),
         dest = new_file,
         encoding = 'utf-8'
     )
 
+    # close file once data written
     new_file.close()
     
 
