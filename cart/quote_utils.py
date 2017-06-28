e9fc1d36d4e9df9f98364ec9f3b6ef8d4b681733
MicrobesNG <microbesng@COLMDS-C02QR0BR.local>
2017-06-28 12:08:29 +0100

    basic quote pdf generation

--- cart/quote_utils.py
+++ cart/quote_utils.py
@@ -5,12 +5,35 @@ from . models import Quote
 from io import BytesIO
 from reportlab.pdfgen import canvas
 from xhtml2pdf import pisa
+import cStringIO as StringIO
 
 
-def generate_quote_pdf(quote, template_path):
+def generate_quote_pdf(quote):
 
     
-    pass
+    context_dictionary = {
+        "quote": quote
+    }
+
+    template = get_template("pdfs/quote.html")
+    context = Context(context_dictionary)
+    html = template.render(context_dictionary)
+
+    result = StringIO.StringIO()
+
+    pdf = pisa.pisaDocument(StringIO.StringIO(html.encode("ISO-8859-1")), result)
+
+    new_file = open("quotes/test.pdf", "w")
+    
+    pisaStatus = pisa.CreatePDF(
+        html.encode('utf-8'),
+        dest = new_file,
+        encoding = 'utf-8'
+    )
+
+    new_file.close()
+    
+
 
 def send_quote(quote):
      pass
