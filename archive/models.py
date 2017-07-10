1ecff8cc8e451646a07dd71b51ec03b534151fe3
MicrobesNG <microbesng@COLMDS-C02QR0BR.local>
2017-07-10 09:45:50 +0100

    remove organisation from strain and work on login page for management

--- archive/models.py
+++ archive/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 from datetime import datetime
-from userprofile.models import Organisation
+
 import json
 
 # taxonomical models
@@ -79,7 +79,6 @@ class Strain(models.Model):
     name = models.CharField(max_length = 100)
     strain_type = models.CharField(max_length = 1, choices = STRAIN_TYPES, default = "D")
     date_added = models.DateTimeField(default = datetime.now)
-    organisation = models.ForeignKey(Organisation, null = True)
     data_url = models.URLField(null = True)
     number_of_sales = models.IntegerField(default = 0)
 
