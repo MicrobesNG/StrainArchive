5f539933aa8a9470004429881e5770b1d3c08b74
MicrobesNG <microbesng@COLMDS-C02QR0BR.local>
2017-07-10 14:00:12 +0100

    basic management profile data

--- management/models.py
+++ management/models.py
@@ -2,7 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
+class ManagementUserProfile(models.Model):
+
+    first_name = models.CharField(max_length = 20)
+    last_name = models.CharField(max_length = 20)
+    user = models.OneToOneField(User)
