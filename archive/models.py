482612559058dd1e2952f231621d5c5b214e9b07
MicrobesNG <microbesng@COLMDS-C02QR0BR.local>
2017-07-13 10:09:42 +0100

    remove old taxonomic models

--- archive/models.py
+++ archive/models.py
@@ -8,62 +8,6 @@ from datetime import datetime
 
 import json
 
-# # taxonomical models
-# class Family(models.Model):
-#     name = models.CharField(max_length = 100)    
-#     # other family data fields?
-
-
-#     def to_dict(self):
-#         output_dict = {
-#             "name": self.name,
-#             "pk": self.pk,
-#             "genera": []
-#         }
-
-#         for genus in self.genus_set.all():
-
-#             output_dict["genera"].append(genus.to_dict())
-
-#         return output_dict
-
-
-#     def to_json_string(self):
-
-#         return json.dumps(self.to_dict())
-
-# class Genus(models.Model):
-#     name = models.CharField(max_length = 100)
-#     family = models.ForeignKey(Family, null = True)
-#     # other genus data fields?
-
-
-#     def to_dict(self):
-#         output_dict = {
-#             "name": self.name,
-#             "pk": self.pk,
-#             "species": []
-#         }
-
-#         for species in self.species_set.all():
-#             output_dict["species"].append(
-#                 {
-#                     "name": species.name,
-#                     "pk": species.pk,
-#                 }
-#             )
-
-#         return output_dict
-    
-
-#     def to_json_string(self):
-
-#         return json.dumps(self.to_dict())
-
-# class Species(models.Model):
-#     name = models.CharField(max_length = 100)
-#     genus = models.ForeignKey(Genus, null = True)
-#     # other species data fields? 
 
 
 
